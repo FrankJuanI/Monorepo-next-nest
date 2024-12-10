7e4cffc494f08e61cf479c96a7239b2f184e4e4e
Juan Frank <juan.frank@gm2dev.com>
2024-12-10 13:24:14 -0300

    feat: create user entity crud

---
+++ apps/api/src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string
+  @Prop()
+  lastname? : string
+  @Prop()
+  email?: string
+
+}
+
+export const CatSchema = SchemaFactory.createForClass(User);
