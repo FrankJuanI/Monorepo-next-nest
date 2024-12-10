da4c2eda9911c990c5e4171bb96dd71378501f31
Juan Frank <juan.frank@gm2dev.com>
2024-12-10 13:24:36 -0300

    chore: remove habit

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
-import { HabitModule } from './habit/habit.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongoadmin:secret@localhost:27017/miapp?authSource=admin'), TaskModule, UserModule, HabitModule],
+  imports: [MongooseModule.forRoot('mongodb://mongoadmin:secret@localhost:27017/miapp?authSource=admin'), TaskModule, UserModule],
   providers: [],
 })
 
