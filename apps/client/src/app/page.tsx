45d4a11f5dcec9c4f0ec66b341f1e3b633011ecd
FrankJuanI <juan.frank@gm2dev.com>
2024-12-11 18:47:32 -0300

    chore: update import

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./ui/styles/page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main className={styles.main} >
         <Image
           className={styles.logo}
           src="/next.svg"
